737264b3afa68904cf718fd0da9a4fb58c7606eb
afsana-haque <afsana.haque3301@gmail.com>
2023-05-21 15:11:22 +0600

    added all toy

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-//middleware
+const app = express();
+require('dotenv').config();
+
+
 app.use(cors());
 app.use(express.json());
-
 
 
 
@@ -28,7 +28,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
+
+    const dollCollection = client.db('dollCollection').collection('doll');
+
+
+
+    app.get('/', async(req, res) =>{
+      res.send("toy island server is running");
+    })
+
+    app.get('/allToy', async(req, res) =>{
+      const result = await dollCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/toyByCategory', async(req, res) =>{
+      const subCategory = req.query?.subCategory;
+      const query = {category: subCategory}
+      const result = await dollCollection.find(query).toArray();
+      res.send(result);
+    })
+
+  
+    app.post('/addToy', async(req, res) =>{
+      const body = req.body;
+      const result = await dollCollection.insertOne(body);
+      res.send(result);
+    })
+
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -41,10 +71,7 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) => {
-    res.send("server is running")
-})
 
 app.listen(port, () => {
-    console.log(` Server is running on port ${port}`)
+  console.log(`server is running on ${port}`)
 })
