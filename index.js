4b93f2c58f817c84aa15aa732481f7a54ea00a39
afsana-haque <afsana.haque3301@gmail.com>
2023-05-21 16:49:34 +0600

    added allToy Id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -40,6 +40,13 @@ async function run() {
 
     app.get('/allToy', async(req, res) =>{
       const result = await dollCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/allToy/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result= await dollCollection.findOne(query);
       res.send(result);
     })
 
